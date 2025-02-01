69087b9ffdbe319436e9e1acb501af93cee03e96
Robsutar <robsonsutar@gmail.com>
2025-02-01 19:27:08 -0300

    Define resource pack socket address

--- src/main/java/com/robsutar/rnu/RNUConfig.java
+++ src/main/java/com/robsutar/rnu/RNUConfig.java
@@ -2,12 +2,16 @@ package com.robsutar.rnu;
 
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.bukkit.Bukkit;
 import org.bukkit.configuration.ConfigurationSection;
 import org.jetbrains.annotations.Nullable;
 
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
 import java.util.Objects;
 
-public record RNUConfig(int port,
+public record RNUConfig(InetSocketAddress address,
                         Component prompt,
                         @Nullable Component kickOnRefuseMessage,
                         @Nullable Component kickOnFailMessage) {
@@ -20,9 +24,27 @@ public record RNUConfig(int port,
                     Define it in plugins/ResourcePackNoUpload/config.yml
                     Make sure to open this port to the players.
                     """);
-        
+        var port = raw.getInt("port");
+
+        String addressStr;
+        var addressRaw = raw.getString("serverAddress");
+        if (addressRaw != null) {
+            addressStr = addressRaw;
+        } else {
+            var definedIp = Bukkit.getIp();
+            if (!definedIp.isEmpty()) {
+                addressStr = definedIp;
+            } else {
+                try {
+                    addressStr = InetAddress.getLocalHost().getHostAddress();
+                } catch (UnknownHostException e) {
+                    throw new IllegalArgumentException("Failed to get server address from program ipv4.");
+                }
+            }
+        }
+
         return new RNUConfig(
-                raw.getInt("port"),
+                new InetSocketAddress(addressStr, port),
                 mm.deserialize(Objects.requireNonNull(raw.getString("prompt"))),
                 raw.get("kickOnRefuseMessage") instanceof String s1 ? mm.deserialize(s1) : null,
                 raw.get("kickOnFailMessage") instanceof String s1 ? mm.deserialize(s1) : null
