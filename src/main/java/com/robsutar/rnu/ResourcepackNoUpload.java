597c3707a5f84879803f788bac23cb297dad5ce1
Robsutar <robsonsutar@gmail.com>
2025-02-01 09:27:58 -0300

    Initial commit

---
+++ src/main/java/com/robsutar/rnu/ResourcepackNoUpload.java
@@ -0,0 +1,17 @@
+package com.robsutar.rnu;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ResourcepackNoUpload extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
