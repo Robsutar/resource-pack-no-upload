161d8f18262f145cd1396965bd28508599aa339a
Robsutar <robsonsutar@gmail.com>
2025-02-01 18:23:35 -0300

    Load config from file

--- src/main/java/com/robsutar/rnu/RNUConfig.java
+++ src/main/java/com/robsutar/rnu/RNUConfig.java
@@ -1,9 +1,31 @@
 package com.robsutar.rnu;
 
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.bukkit.configuration.ConfigurationSection;
 import org.jetbrains.annotations.Nullable;
 
-public record RNUConfig(int port, Component prompt, @Nullable Component kickOnRefuseMessage,
-                        @Nullable Component kickOnFailMessage) {
+import java.util.Objects;
 
+public record RNUConfig(int port,
+                        Component prompt,
+                        @Nullable Component kickOnRefuseMessage,
+                        @Nullable Component kickOnFailMessage) {
+    public static RNUConfig deserialize(ConfigurationSection raw) throws IllegalArgumentException {
+        var mm = MiniMessage.miniMessage();
+
+        if (raw.get("port") == null)
+            throw new IllegalArgumentException("""
+                    Port undefined in configuration!
+                    Define it in plugins/ResourcePackNoUpload/config.yml
+                    Make sure to open this port to the players.
+                    """);
+        
+        return new RNUConfig(
+                raw.getInt("port"),
+                mm.deserialize(Objects.requireNonNull(raw.getString("prompt"))),
+                raw.get("kickOnRefuseMessage") instanceof String s1 ? mm.deserialize(s1) : null,
+                raw.get("kickOnFailMessage") instanceof String s1 ? mm.deserialize(s1) : null
+        );
+    }
 }
--- src/main/java/com/robsutar/rnu/ResourcePackNoUpload.java
+++ src/main/java/com/robsutar/rnu/ResourcePackNoUpload.java
@@ -1,17 +1,42 @@
 package com.robsutar.rnu;
 
 import net.kyori.adventure.resource.ResourcePackInfo;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.Nullable;
 
+import java.io.File;
+import java.io.IOException;
+
 public final class ResourcePackNoUpload extends JavaPlugin {
+    private RNUConfig config;
 
     @Override
     public void onEnable() {
+        load();
+    }
+
+    public void load() {
+        var folder = getDataFolder();
+        if (!folder.exists()&&!folder.mkdir())
+            throw new IllegalStateException("Failed to create plugin folder");
+
+        var configRaw = new YamlConfiguration();
+        var configFile = new File(folder, "config.yml");
+        if (!configFile.exists()) {
+            saveResource("config.yml", false);
+        }
+        try {
+            configRaw.load(configFile);
+        } catch (IOException | InvalidConfigurationException e) {
+            throw new IllegalStateException("Failed to load configuration file");
+        }
+        config = RNUConfig.deserialize(configRaw);
     }
 
     public RNUConfig config() {
-
+        return config;
     }
 
     public @Nullable ResourcePackInfo actualResourcePackInfo() {
