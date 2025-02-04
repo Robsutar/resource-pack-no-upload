a4af772017ab7c299807f2db005fe845bbe1e732
Robsutar <robsonsutar@gmail.com>
2025-02-03 22:43:16 -0300

    Add ResourcePackLoader.Merged
    Merged loader runs nested loaders, merging their resource packs.

--- src/main/java/com/robsutar/rnu/ResourcePackLoader.java
+++ src/main/java/com/robsutar/rnu/ResourcePackLoader.java
@@ -5,18 +5,45 @@ import org.bukkit.configuration.ConfigurationSection;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.Objects;
+import java.util.*;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public interface ResourcePackLoader {
-    byte[] load() throws Exception;
+    Map<String, File> appendFiles() throws Exception;
+
+    default byte[] load() throws Exception {
+        try (var byteOut = new ByteArrayOutputStream(); var zipOut = new ZipOutputStream(byteOut)) {
+            var appended = appendFiles();
+
+            for (var entry : appended.entrySet()) {
+                var file = entry.getValue();
+                var name = entry.getKey();
+                if (!file.isFile()) throw new IllegalArgumentException();
+
+                var fileIn = new FileInputStream(file);
+                var zipEntry = new ZipEntry(name);
+                zipOut.putNextEntry(zipEntry);
+
+                var buffer = new byte[1024];
+                int len;
+                while ((len = fileIn.read(buffer)) > 0) {
+                    zipOut.write(buffer, 0, len);
+                }
+
+                fileIn.close();
+            }
+
+            zipOut.close();
+            return byteOut.toByteArray();
+        }
+    }
 
     static ResourcePackLoader deserialize(ConfigurationSection raw) {
         var type = Objects.requireNonNull(raw.getString("type"), "Missing loader type");
         return switch (type) {
             case "Manual" -> new Manual(raw);
+            case "Merged" -> new Merged(raw);
             default -> throw new IllegalArgumentException("Invalid loader type: " + type);
         };
     }
@@ -29,59 +56,48 @@ public interface ResourcePackLoader {
         }
 
         @Override
-        public byte[] load() throws Exception {
-            if (!folder.exists()) throw new IllegalArgumentException("Directory not found: " + folder);
+        public Map<String, File> appendFiles() throws Exception {
+            if (!folder.exists()) throw new Exception("Directory not found: " + folder);
+            if (!folder.isDirectory()) throw new Exception("File is not directory: " + folder);
 
-            try (var byteOut = new ByteArrayOutputStream(); var zipOut = new ZipOutputStream(byteOut)) {
-                var files = folder.listFiles();
-                if (files != null) {
-                    for (var file : files) {
-                        if (file.isFile()) {
-                            var fileIn = new FileInputStream(file);
-                            var zipEntry = new ZipEntry(file.getName());
-                            zipOut.putNextEntry(zipEntry);
-
-                            var buffer = new byte[1024];
-                            int len;
-                            while ((len = fileIn.read(buffer)) > 0) {
-                                zipOut.write(buffer, 0, len);
-                            }
-
-                            fileIn.close();
-                        } else if (file.isDirectory()) {
-                            addDirectoryToZip(zipOut, file, file.getName() + "/");
-                        }
-                    }
-                } else {
-                    throw new IllegalArgumentException("Not an directory: " + folder);
-                }
-
-                zipOut.close();
-                return byteOut.toByteArray();
-            }
+            var output = new HashMap<String, File>();
+            appendDirectory(output, folder, "");
+            return output;
         }
 
-        private static void addDirectoryToZip(ZipOutputStream zipOut, File directory, String parentPath) throws IOException {
+        private static void appendDirectory(Map<String, File> output, File directory, String parentPath) {
             var files = directory.listFiles();
             if (files != null) {
                 for (var file : files) {
                     if (file.isFile()) {
-                        var fileIn = new FileInputStream(file);
-                        var zipEntry = new ZipEntry(parentPath + file.getName());
-                        zipOut.putNextEntry(zipEntry);
-
-                        var buffer = new byte[1024];
-                        int len;
-                        while ((len = fileIn.read(buffer)) > 0) {
-                            zipOut.write(buffer, 0, len);
-                        }
-
-                        fileIn.close();
+                        output.put(parentPath + file.getName(), file);
                     } else if (file.isDirectory()) {
-                        addDirectoryToZip(zipOut, file, parentPath + file.getName() + "/");
+                        appendDirectory(output, file, parentPath + file.getName() + "/");
                     }
                 }
             }
+        }
+    }
+
+    class Merged implements ResourcePackLoader {
+        private final List<ResourcePackLoader> loaders = new ArrayList<>();
+
+        public Merged(ConfigurationSection raw) {
+            for (var loaderMap : raw.getMapList("loaders")) {
+                var loaderRaw = raw.createSection("DISPOSABLE_SECTION", loaderMap);
+                loaders.add(deserialize(loaderRaw));
+            }
+            raw.set("DISPOSABLE_SECTION", null);
+        }
+
+        @Override
+        public Map<String, File> appendFiles() throws Exception {
+            var output = new HashMap<String, File>();
+            for (var i = loaders.size() - 1; i >= 0; i--) {
+                var loader = loaders.get(i);
+                output.putAll(loader.appendFiles());
+            }
+            return output;
         }
     }
 }
