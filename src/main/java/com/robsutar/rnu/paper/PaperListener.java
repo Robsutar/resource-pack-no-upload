81f3596c802d4289ba753c71e0f7795c60466151
Robsutar <robsonsutar@gmail.com>
2025-02-06 04:11:53 -0300

    Use legacy resource pack sending
    This ignores the usage of prompt, and required in resource pack sending, witch does not exist in older versions. For the newer versions, this will be fixed in the next commit.

--- src/main/java/com/robsutar/rnu/paper/PaperListener.java
+++ src/main/java/com/robsutar/rnu/paper/PaperListener.java
@@ -3,14 +3,13 @@ package com.robsutar.rnu.paper;
 import com.robsutar.rnu.ResourcePackInfo;
 import com.robsutar.rnu.ResourcePackNoUpload;
 import com.robsutar.rnu.ResourcePackState;
-import net.kyori.adventure.resource.ResourcePackInfo;
-import net.kyori.adventure.resource.ResourcePackRequest;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerResourcePackStatusEvent;
 
 public class PaperListener implements Listener {
     private final ResourcePackNoUpload plugin;
@@ -20,37 +19,14 @@ public class PaperListener implements Listener {
     }
 
     private void sendPack(Player player, ResourcePackInfo resourcePackInfo) {
-        var config = plugin.config();
-
-        player.clearResourcePacks();
-        var resourcePack = ResourcePackRequest.resourcePackRequest()
-                .callback((uuid, status, audience) -> {
-                    if (!status.intermediate()) switch (status) {
-                        case DECLINED -> {
-                            if (config.kickOnRefuseMessage() != null) {
-                                player.kick(config.kickOnRefuseMessage());
-                            }
-                        }
-                        case INVALID_URL, FAILED_DOWNLOAD, FAILED_RELOAD, DISCARDED -> {
-                            if (config.kickOnFailMessage() != null) {
-                                player.kick(config.kickOnFailMessage().replaceText(
-                                        (c) -> c.matchLiteral("<error_code>").replacement(status.name()))
-                                );
-                            }
-                        }
-                        case SUCCESSFULLY_LOADED -> {
-                        }
-                        // Intermediates called here, but was checked previously.
-                        default -> throw new IllegalStateException(
-                                "Invalid state, " + plugin.getName() + " is outdated?"
-                        );
-                    }
-                })
-                .replace(true)
-                .prompt(plugin.config().prompt())
-                .packs(resourcePackInfo);
-
-        player.sendResourcePacks(resourcePack);
+        player.setResourcePack(resourcePackInfo.uri().toString(), resourcePackInfo.hash());
+//        player.setResourcePack(
+//                resourcePackInfo.id(),
+//                resourcePackInfo.uri().toString(),
+//                resourcePackInfo.hash(),
+//                plugin.config().prompt(),
+//                false
+//        );
     }
 
     @EventHandler(priority = EventPriority.MONITOR)
@@ -59,6 +35,35 @@ public class PaperListener implements Listener {
 
         if (plugin.resourcePackState() instanceof ResourcePackState.Loaded loaded) {
             sendPack(player, loaded.resourcePackInfo());
+        }
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onPlayerResourcePackStatus(PlayerResourcePackStatusEvent event) {
+        var config = plugin.config();
+        var player = event.getPlayer();
+        var status = event.getStatus();
+
+        switch (status.name()) {
+            // Intermediates called here.
+            case "ACCEPTED", "DOWNLOADED" -> {
+            }
+            case "DECLINED" -> {
+                if (config.kickOnRefuseMessage() != null) {
+                    player.kickPlayer(config.kickOnRefuseMessage());
+                }
+            }
+            case "INVALID_URL", "FAILED_DOWNLOAD", "FAILED_RELOAD", "DISCARDED" -> {
+                if (config.kickOnFailMessage() != null) {
+                    player.kickPlayer(config.kickOnFailMessage().replace("<error_code>", status.name()));
+                }
+            }
+            case "SUCCESSFULLY_LOADED" -> {
+                // All ok.
+            }
+            default -> throw new IllegalStateException(
+                    "Invalid state, " + plugin.getName() + " is outdated?"
+            );
         }
     }
 
