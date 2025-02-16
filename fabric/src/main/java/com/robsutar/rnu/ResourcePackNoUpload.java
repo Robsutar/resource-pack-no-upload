c52ee4e131bbd78cd033ea4ea69473dd7bc117a2
Robsutar <robsonsutar@gmail.com>
2025-02-16 09:10:02 -0300

    Define base fabric server mod for fabric module

---
+++ fabric/src/main/java/com/robsutar/rnu/ResourcePackNoUpload.java
@@ -0,0 +1,11 @@
+package com.robsutar.rnu;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ResourcePackNoUpload implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+        System.out.println("RNU here!");
+    }
+}
