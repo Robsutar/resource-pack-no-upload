25e88e9a07c4e2cac0c6af0e8f7a02e33cfb11fe
Robsutar <robsonsutar@gmail.com>
2025-02-18 18:34:44 -0300

    Define plugin config loading wrapper

--- fabric/src/main/java/com/robsutar/rnu/ResourcePackNoUpload.java
+++ fabric/src/main/java/com/robsutar/rnu/ResourcePackNoUpload.java
@@ -2,9 +2,23 @@ package com.robsutar.rnu;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.server.level.ServerPlayer;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.file.Files;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ResourcePackNoUpload implements ModInitializer {
+    private final Logger logger = Logger.getLogger(ResourcePackNoUpload.class.getName());
 
     @Override
     public void onInitialize() {
@@ -12,5 +26,67 @@ public class ResourcePackNoUpload implements ModInitializer {
             ServerPlayer player = handler.player;
             player.sendTexturePack("url", "hash");
         });
+    }
+
+    public String getId() {
+        return "resourcepacknoupload";
+    }
+
+    public File getDataFolder() {
+        return FabricLoader.getInstance().getConfigDir().resolve(getId()).toFile();
+    }
+
+    public void saveResource(@NotNull String resourcePath, boolean replace) {
+        if (resourcePath.isEmpty()) {
+            throw new IllegalArgumentException("ResourcePath cannot be null or empty");
+        }
+
+        File dataFolder = getDataFolder();
+
+        resourcePath = resourcePath.replace('\\', '/');
+        InputStream in = getResource(resourcePath);
+        if (in == null) {
+            throw new IllegalArgumentException("The embedded resource '" + resourcePath + "' cannot be found in " + getId());
+        }
+
+        File outFile = new File(dataFolder, resourcePath);
+        int lastIndex = resourcePath.lastIndexOf('/');
+        File outDir = new File(dataFolder, resourcePath.substring(0, Math.max(lastIndex, 0)));
+
+        if (!outDir.exists() && !outDir.mkdirs()) throw new IllegalStateException();
+
+        try {
+            if (!outFile.exists() || replace) {
+                OutputStream out = Files.newOutputStream(outFile.toPath());
+                byte[] buf = new byte[1024];
+                int len;
+                while ((len = in.read(buf)) > 0) {
+                    out.write(buf, 0, len);
+                }
+                out.close();
+                in.close();
+            } else {
+                logger.log(Level.WARNING, "Could not save " + outFile.getName() + " to " + outFile + " because " + outFile.getName() + " already exists.");
+            }
+        } catch (IOException ex) {
+            logger.log(Level.SEVERE, "Could not save " + outFile.getName() + " to " + outFile, ex);
+        }
+    }
+
+    @Nullable
+    public InputStream getResource(@NotNull String filename) {
+        try {
+            URL url = getClass().getClassLoader().getResource(filename);
+
+            if (url == null) {
+                return null;
+            }
+
+            URLConnection connection = url.openConnection();
+            connection.setUseCaches(false);
+            return connection.getInputStream();
+        } catch (IOException ex) {
+            return null;
+        }
     }
 }
