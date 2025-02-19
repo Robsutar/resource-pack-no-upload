90d8da4c0bf887a8cc29490c50884ae90c76fe9a
Robsutar <robsonsutar@gmail.com>
2025-02-18 21:13:22 -0300

    Add placeholder event listener

---
+++ fabric/src/main/java/com/robsutar/rnu/fabric/FabricListener.java
@@ -0,0 +1,13 @@
+package com.robsutar.rnu.fabric;
+
+import com.robsutar.rnu.ResourcePackNoUpload;
+
+public class FabricListener {
+    public FabricListener(ResourcePackNoUpload plugin) {
+        
+    }
+
+    public void register() {
+
+    }
+}
